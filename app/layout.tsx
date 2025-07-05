e8411b95c81596e795259a70a89cb961691020af
Taylor Stein <taylor@engagedmd.com>
2025-07-05 14:28:06 -0400

    Update PWA with custom app icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,9 +54,15 @@ export default function ({ children }: Readonly<{ children: React.ReactNode }>) 
     <html lang="en">
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/icon-512x512.png" />
+        <link rel="apple-touch-icon" href="/icons/icon-152x152.png" />
+        <link rel="icon" type="image/png" sizes="72x72" href="/icons/icon-72x72.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/icons/icon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="128x128" href="/icons/icon-128x128.png" />
+        <link rel="icon" type="image/png" sizes="144x144" href="/icons/icon-144x144.png" />
+        <link rel="icon" type="image/png" sizes="152x152" href="/icons/icon-152x152.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icons/icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="384x384" href="/icons/icon-384x384.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/icons/icon-512x512.png" />
         <meta name="theme-color" content="#000000" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
